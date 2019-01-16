f39b923acf014fa041fc469f18fe4ed2977b109b
Cory House <housecor@gmail.com>
2019-01-16 09:49:40 -0600

    Simple hello world

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,29 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Users from "./Users";
+import { UserEntity } from "./model/userEntity";
+import "./App.css";
 
-class App extends Component {
+interface State {
+  users: UserEntity[];
+}
+
+interface Props {}
+
+class App extends Component<Props, State> {
+  state = {
+    users: [{ id: 1, name: "Cory" }, { id: 2, name: "Bob" }]
+  };
+
+  // handleClickDelete = id: Number => {
+  //   this.setState(prevState => {
+  //     return { users: [...prevState.filter(u => u.id !== id)] };
+  //   });
+  // };
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Users users={this.state.users} />
       </div>
     );
   }
---
+++ src/model/userEntity.ts
@@ -0,0 +1,4 @@
+export interface UserEntity {
+  id: number;
+  name: string;
+}
