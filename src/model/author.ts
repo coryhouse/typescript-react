fe337535a283eeb9d75ee5ac28bf5436df14ffca
Cory House <housecor@gmail.com>
2019-01-17 09:24:51 -0600

    Add author and course models

---
+++ src/model/author.ts
@@ -0,0 +1,4 @@
+export interface Author {
+  id: Number;
+  name: String;
+}
---
+++ src/model/course.ts
@@ -0,0 +1,7 @@
+export interface Course {
+  id: Number;
+  title: String;
+  slug: String;
+  authorId: Number;
+  category: string;
+}
